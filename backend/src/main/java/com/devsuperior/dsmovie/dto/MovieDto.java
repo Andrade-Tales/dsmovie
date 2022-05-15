2536c2958324f4ca4e3e0f3dd660e1f0dfc2a0ae
Andrade-Tales <selatedardna.1476@gmail.com>
2022-05-15 17:30:52 -0300

    Find movies

---
+++ backend/src/main/java/com/devsuperior/dsmovie/dto/MovieDto.java
@@ -0,0 +1,106 @@
+package com.devsuperior.dsmovie.dto;
+
+import com.devsuperior.dsmovie.entity.Movie;
+
+public class MovieDto {
+	
+	private Long id;
+	private String title;
+	private Double score;
+	private Integer count;
+	private String image;	
+	
+	public MovieDto() {
+		
+	}
+
+	public MovieDto(Long id, String title, Double score, Integer count, String image) {
+		this.id = id;
+		this.title = title;
+		this.score = score;
+		this.count = count;
+		this.image = image;
+	}
+	
+	public MovieDto(Movie movie) {
+		id = movie.getId();
+		title = movie.getTitle();
+		score = movie.getScore();
+		count = movie.getCount();
+		image = movie.getImage();
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the score
+	 */
+	public Double getScore() {
+		return score;
+	}
+
+	/**
+	 * @param score the score to set
+	 */
+	public void setScore(Double score) {
+		this.score = score;
+	}
+
+	/**
+	 * @return the count
+	 */
+	public Integer getCount() {
+		return count;
+	}
+
+	/**
+	 * @param count the count to set
+	 */
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+
+	/**
+	 * @return the image
+	 */
+	public String getImage() {
+		return image;
+	}
+
+	/**
+	 * @param image the image to set
+	 */
+	public void setImage(String image) {
+		this.image = image;
+	}
+	
+	
+
+	
+}
